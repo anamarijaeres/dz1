ca394c2744df215871da65b1edd82e3cbb4b3d89
Ana Marija <ae50637@fer.hr>
2020-05-11 04:58:56 +0200

    Initial commit

---
+++ QuizApp1/Model/Quiz.swift
@@ -0,0 +1,41 @@
+//
+//  Quiz.swift
+//  QuizApp1
+//
+//  Created by five on 10/05/2020.
+//  Copyright © 2020 five. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Quiz{
+    let title: String
+    let image:String?
+    let category:String
+    var allquestions: [Question]=[]
+ 
+    init?(json:Any){
+        print("parsing")
+        if let jsonDict=json as? [String:Any],
+            let title=jsonDict["title"] as? String,
+            let image=jsonDict["image"] as? String,
+            let category=jsonDict["category"] as? String,
+            let questions=jsonDict["questions"] as? [[String:Any]]{
+            for q in questions{
+                let q=Question(json: q)
+                print("Ovo su questioni:\(q.correctAnswer)  "+"\(q.answers)"+"\(q.question)")
+                self.allquestions.append(q)
+            }
+                self.title=title
+                self.image=image
+                self.category=category
+    
+                print("\(self.title)  "+"\(self.image)"+"\(self.category)")
+        
+        }else{
+            return nil
+        }
+    }
+    
+}
